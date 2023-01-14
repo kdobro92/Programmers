3cf69e477dd107b446fb7a79a3872a44aaa0c238
kdobro92 <kdobro92@gmail.com>
2023-01-14 21:37:51 +0900

    day01

---
+++ level_0/calculate_string.js
@@ -0,0 +1,16 @@
+function solution(my_string) {
+  const splited = my_string.split(" ");
+  let ans = Number(splited[0]);
+
+  splited.forEach((item, index) => {
+    if (item === "+") {
+      ans += Number(splited[index + 1]);
+    }
+
+    if (item === "-") {
+      ans -= Number(splited[index + 1]);
+    }
+  });
+
+  return ans;
+}
